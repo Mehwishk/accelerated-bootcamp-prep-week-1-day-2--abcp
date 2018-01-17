d2bf7140865c33964b3f2c49df51f8cf1bdaaee5
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-17 17:50:41 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,34 +1,67 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ var newArray=[element,...array];
+return (newArray);
+
 }
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+array.unshift(element);
+return array;
+
+var games = ["ball","bat","glove"];
+  games.unshift("base");
+  return games
 }
 
 function addElementToEndOfArray(array, element) {
-
+var colors = ["blue"]
+//do return [...array,element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push
 }
 
 function accessElementInArray(array, index) {
-
+var planets= ["mars","jupiter","pluto"];
+planet [0]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+var planets= ["mars","jupiter","pluto"];
+planets.shift(1)
+//array.shift(elment)
+//return array
 }
 
 function removeElementFromBeginningOfArray(array) {
 }
+return array.slice(1)
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop(element)
+  return array
 
 }
 
 function removeElementFromEndOfArray(array) {
+return array.slice(0, array.length - 1)
+}
+// CHALLENGE: remove an element from the middle of the array/at a given index 
+// and return a COPY of the original array, minus that element
 
+function nonDestructivelyRemoveAtIndex(array, index) {
+// using slice _and_ spread??
+}
+
+
+function ifGreater(n){
+if ( n=>10){
+    console.log(n + " is greater than 10")}
+    else {
+      console.log ("Wrong no!")
+    }
 }
